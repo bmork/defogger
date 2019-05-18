9f0a6180ee28b07148f94e8e9b6547d8e98b5940
Bjørn Mork <bjorn@mork.no>
2019-05-18 10:08:07 +0200

    android wip: build mainactiviy only
    Signed-off-by: Bjørn Mork <bjorn@mork.no>

--- Defogger/src/no/mork/android/defogger/MainActivity.java
+++ Defogger/src/no/mork/android/defogger/MainActivity.java
@@ -3,10 +3,17 @@ package no.mork.android.defogger;
 import android.app.Activity;
 import android.os.Bundle;
 
+import no.mork.android.defogger.ClientActivity;
+
 public class MainActivity extends Activity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+
+      //      binding.launchClientButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+      //									  ClientActivity.class)));
+
+
    }
 }
